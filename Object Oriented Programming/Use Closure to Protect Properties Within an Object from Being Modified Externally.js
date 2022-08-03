e3fe7ab9b3efb289966ee09df55dddf0d71b835a
sauhardM <mishra.sauhard@gmail.com>
2022-08-04 01:38:12 +0530

    Use Closure to Protect Properties Within an Object from Being Modified Externally

---
+++ Object Oriented Programming/Use Closure to Protect Properties Within an Object from Being Modified Externally.js
@@ -0,0 +1,9 @@
+// Use Closure to Protect Properties Within an Object from Being Modified Externally
+
+function Bird() {
+    let weight = 15;
+  
+    this.getWeight = function(){ return weight};
+  
+  
+  }
