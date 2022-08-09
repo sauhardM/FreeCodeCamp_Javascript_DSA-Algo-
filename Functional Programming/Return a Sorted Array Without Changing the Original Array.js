71fe71cfa97289e8de088aeabb4ef6564b08bf02
sauhardM <mishra.sauhard@gmail.com>
2022-08-09 18:36:07 +0530

    Return a Sorted Array Without Changing the Original Array

---
+++ Functional Programming/Return a Sorted Array Without Changing the Original Array.js
@@ -0,0 +1,13 @@
+// Return a Sorted Array Without Changing the Original Array
+
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+
+      let newArr = arr.slice();
+      return newArr.sort((a,b) => a - b);
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
